05778c15ac8ddef7668b1674536d5b983c35b9ea
Kwon812 <tre723@naver.com>
2026-05-14 10:34:29 +0900

    fix: 비로그인 사용자 전시회 입장 불가능한 버그 수정

--- src/components/galleryExhibition/threejs/Scene.tsx
+++ src/components/galleryExhibition/threejs/Scene.tsx
@@ -5,15 +5,18 @@ import { GalleryUIArtworkProps } from '@/types/gallery';
 import { createWalls, generateGalleryWalls } from '@/lib/gallery/createWalls';
 import Room from '@/components/galleryExhibition/threejs/Room';
 import Player from '@/components/galleryExhibition/threejs/Player';
+import { User } from '@supabase/supabase-js';
 
 export default function Scene2({
   exhibitionId,
   ready,
   init,
+  user,
 }: {
   exhibitionId: string;
   ready: Dispatch<SetStateAction<boolean>>;
   init: GalleryUIArtworkProps[];
+  user: User | null;
 }) {
   // console.log(init)
   const size = 21;
@@ -30,6 +33,7 @@ export default function Scene2({
     <>
       <Canvas shadows camera={{ fov: 50 }}>
         <Room
+          user={user}
           exhibitionId={exhibitionId}
           walls={walls}
           innerWalls={innerWalls}
